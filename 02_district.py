d0419e1d36ef127b24e3c62d9e1419e52190f805
Autolab72 <Autolab72@yandex.ru>
2021-10-21 19:31:28 +0500

    districk ok

--- 02_district.py
+++ 02_district.py
@@ -5,8 +5,25 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+# from district.central_street import house1
+# from district.central_street import house2
+# from district.soviet_street import house1
+# from district.soviet_street import house2
+# import district.central_street.house1.room1
+# import district.central_street.house1.room2
+# import district.central_street.house2.room1
+# import district.central_street.house2.room2
+# import district.soviet_street.house1.room1
+# import district.soviet_street.house1.room2
+# import district.soviet_street.house2.room1
+# import district.soviet_street.house2.room2
+from district.central_street.house1.room1 import folks as c1h1r1
+from district.central_street.house1.room2 import folks as c1h1r2
+from district.central_street.house2.room1 import folks as c1h2r1
+from district.central_street.house2.room2 import folks as c1h2r2
+from district.soviet_street.house1.room1 import folks as s1h1r1
+from district.soviet_street.house1.room2 import folks as s1h1r2
+from district.soviet_street.house2.room1 import folks as s1h2r1
+from district.soviet_street.house2.room2 import folks as s1h2r2
 
-
-
-
+print(c1h1r1 + c1h1r2 + c1h2r1 + c1h2r2 + s1h1r1 + s1h1r2 + s1h2r1 + s1h2r2)
